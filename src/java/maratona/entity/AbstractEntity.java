dbd35884d6c23303c92098cc0fc592a65467e4c8
vitor_gja_ <vitor_gja_@DESKTOP-TTVA28O>
2017-11-27 20:40:51 -0200

    Arquivos Padroes da Configuração.

---
+++ src/java/maratona/entity/AbstractEntity.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maratona.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ *
+ * @author ciro
+ */
+@MappedSuperclass
+public abstract class AbstractEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.id != null ? this.id : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AbstractEntity other = (AbstractEntity) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+        
+}
---
+++ src/java/maratona/util/jpa/JPAEntityManager.java
@@ -0,0 +1,17 @@
+package maratona.util.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAEntityManager {
+   private static EntityManagerFactory emf = null;
+   
+   public static EntityManager getEntityManager() {
+      if (emf == null) {
+         emf = Persistence.
+              createEntityManagerFactory("financasPU");
+      }
+      return emf.createEntityManager();
+   }
+}
